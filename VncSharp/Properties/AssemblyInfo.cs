12e5fa343491378d1ad6c233d7775e73c6b81b69
dpse <mail@dpse.net>
2020-04-21 19:13:58 +0200

    Update assembly information.

--- VncSharp/Properties/AssemblyInfo.cs
+++ VncSharp/Properties/AssemblyInfo.cs
@@ -3,10 +3,10 @@ using System.Reflection;
 [assembly: AssemblyTitle("VncSharp")]
 [assembly: AssemblyDescription(".NET VNC Client Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("David Humphrey")]
+[assembly: AssemblyCompany("David Humphrey et al.")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("GPL 2")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
-[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyVersion("1.3.0")]
